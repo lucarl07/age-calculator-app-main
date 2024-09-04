7fd24c926bb9e62146bb1370318eeddf30f3193f
lucarl07 <junitinho2007@gmail.com>
2024-09-03 22:56:41 -0300

    💄 feat: Base style of the Container component

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,8 +2,14 @@ import styles from "./Container.module.css"
 
 const Container = () => {
   return (
-    <>
-    </>
+    <section className={styles.container}>
+      {/* Top - Birth Date Formulary */}
+      <p>A lonely paragraph...</p>
+
+      {/* Middle - Section Separator */}
+      
+      {/* Bottom - Resulting Age */}
+    </section>
   );
 }
 
