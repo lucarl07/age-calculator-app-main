6710cd32f35add58c2160f87e549cc832b7046aa
lucarl07 <junitinho2007@gmail.com>
2024-09-08 21:55:52 -0300

    💥 fix: Empty input handling at DateForm.jsx

--- src/components/DateForm/DateForm.jsx
+++ src/components/DateForm/DateForm.jsx
@@ -5,7 +5,7 @@ import styles from "./DateForm.module.css"
 import FormField from "./FormField";
 
 // Modules:
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 const DateForm = () => {
   const [ bday, setBday ] = useState('') // Birthday
@@ -17,19 +17,19 @@ const DateForm = () => {
       <FormField.Root>
         <FormField.Label type="day" />
         <FormField.Input type="day" getter={bday} setter={setBday} />
-        { bday.length === 0 && <FormField.Error error="Please delete me afterwards." /> }
+        <FormField.Error error="" />
       </FormField.Root>
       
       <FormField.Root>
         <FormField.Label type="month" />
         <FormField.Input type="month" getter={bmonth} setter={setBmonth} />
-        { bmonth.length === 0 && <FormField.Error error="Please delete me afterwards." /> }
+        <FormField.Error error="" />
       </FormField.Root>
 
       <FormField.Root>
         <FormField.Label type="year" />
         <FormField.Input type="year" getter={byear} setter={setByear} />
-        { byear.length === 0 && <FormField.Error error="Please delete me afterwards." /> }
+        <FormField.Error error="" />
       </FormField.Root>
     </form>
   );
